8cfb91792173cffffbb3daa950adb905ae0ad4c8
Evan Broder <broder@mit.edu>
2008-11-30 14:58:15 -0500

    Initial commit and project description

---
+++ icbing/__init__.py
@@ -0,0 +1,16 @@
+"""
+I Can't Believe It's Not Gmail!
+
+An IMAP server for people who like to control their own data.
+
+ICBING provides the tagging/labelling features of Gimap. Each folder
+represents a tag. Moving messages into that folder adds the tag to the
+message; deleting a message in a folder removes the tag. The contents
+of a folder are all messages with that tag.
+
+For fast access and easy tag management, all e-mail is stored in a
+database.
+
+ICBING also provides a series of scripts for managing messages from
+the command line or procmail.
+"""
