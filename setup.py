7e19ea0d97c341037ca6d9027dcddaa6cbc4600e
Evan Broder <broder@mit.edu>
2008-11-30 15:14:56 -0500

    Initial setup.py file
    The install_requires line is commented out because my twisted
    distribution doesn't include egg-info, and I can't figure out how to
    specify the right dependency

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="icbing",
+    version="0.0.0",
+    description="I Can't Believe It's Not Gmail - A tagging IMAP server",
+    author="Evan Broder",
+    author_email="broder@mit.edu",
+    license="MIT",
+    py_modules=['icbing'],
+    #install_requires=['Twisted_Mail']
+)
