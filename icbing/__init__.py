388bad19565f541b7b8e4c43896af83f06fbc705
Evan Broder <broder@mit.edu>
2008-12-05 13:54:44 -0500

    Document the more complicated mailbox tagging/querying system

--- icbing/__init__.py
+++ icbing/__init__.py
@@ -3,10 +3,14 @@ I Can't Believe It's Not Gmail!
 
 An IMAP server for people who like to control their own data.
 
-ICBING provides the tagging/labelling features of Gimap. Each folder
-represents a tag. Moving messages into that folder adds the tag to the
-message; deleting a message in a folder removes the tag. The contents
-of a folder are all messages with that tag.
+ICBING provides the tagging/labelling features of Gimap. All folders
+are virtual folders. Moving messages into that folder adds some tag to
+the message; deleting a message in a folder removes that tag. The
+contents of a folder are dynamically generated based on a query
+associated with the folder.
+
+By default, a folder corresponds to a tag of the same name, and shows
+all messages with that tag.
 
 For fast access and easy tag management, all e-mail is stored in a
 database.
