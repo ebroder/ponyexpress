e69578a2677003a8160af5a9e9e3cb0bfd54f3a3
Evan Broder <broder@mit.edu>
2009-05-24 16:02:56 -0700

    Tags must be unique.

--- ponyexpress/model/tag.py
+++ ponyexpress/model/tag.py
@@ -6,4 +6,5 @@ class Tag(Base):
     __tablename__ = 'tags'
 
     id = sa.Column(sa.types.Integer, primary_key=True)
-    name = sa.Column(sa.types.Unicode(255), nullable=False, index=True)
+    name = sa.Column(sa.types.Unicode(255), nullable=False, index=True,
+                     unique=True)
