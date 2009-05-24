67206011c86c1dfbc43e44bef89387a72794db29
Evan Broder <broder@mit.edu>
2009-05-24 16:13:47 -0700

    transactional as an argument to sessionmaker is deprecated.

--- ponyexpress/model/__init__.py
+++ ponyexpress/model/__init__.py
@@ -12,7 +12,7 @@ from ponyexpress.model import meta
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
 
-    sm = orm.sessionmaker(autoflush=True, transactional=True, bind=engine)
+    sm = orm.sessionmaker(autoflush=True, autocommit=False, bind=engine)
 
     meta.engine = engine
     meta.Session = orm.scoped_session(sm)
