ff993843a673d3739ce971fac4ba660c71f51ca3
Evan Broder <broder@mit.edu>
2009-05-24 17:18:33 -0700

    Oh right - we depend on SQLAlchemy.

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email="broder@mit.edu",
     license="MIT",
     packages=find_packages(),
-    #install_requires=['Twisted_Mail']
+    #install_requires=['Twisted_Mail', 'SQLAlchemy>=0.5.0']
 )
