5d1da125eb18fda86f5ac7a5ff0bd11eedf59759
Evan Broder <broder@mit.edu>
2009-05-29 21:04:13 -0700

    Re-implement __parseSet for the new non-super-mailbox world order.
    Its handling of MessageSet for sequence number IDs is...kind of really
    retarded. It should probably loop over messages.ranges instead and
    take advantage of query slicing.

--- ponyexpress/model/tag.py
+++ ponyexpress/model/tag.py
@@ -21,3 +21,44 @@ class Tag(Base):
     tag_messages = relation(MessageTag, backref='tag')
     messages = association_proxy('tag_messages', 'message',
                                  creator=(lambda x: MessageTag(message=x)))
+
+    def __parseSet(self, messages, uid):
+        """
+        Convert any MessageSet to something more generally usable
+
+        @type C{twisted.mail.imap4.MessageSet}
+        @param messages: An object representifying the identifiers to
+        iterate over, such as the one passed to
+        C{twisted.mail.imap4.fetch}
+
+        @type C{bool}
+        @param uid: Whether the IDs in messages are UIDs or message
+        sequence IDs.
+
+        @rtype: An iterable of UIDs
+        """
+        if uid:
+            last = meta.Session.query(sa.func.max(MessageTag.id)).\
+                filter(MessageTag.tag_id==self.id).scalar()
+            if last is None:
+                return []
+            else:
+                messages.last = last
+                return messages
+        else:
+            last = meta.Session.query(MessageTag).\
+                filter(MessageTag.tag_id==self.id).\
+                count()
+            if last is 0:
+                return []
+            else:
+                messages.last = last
+                # TODO: Make this not as retardedly inefficient
+                #
+                # It'll currently generate one query per message
+                # sequence number.
+                for m in messages:
+                    yield meta.Session.query(MessageTag.id).\
+                        filter(MessageTag.tag_id==self.id).\
+                        offset(m - 1).\
+                        scalar()
