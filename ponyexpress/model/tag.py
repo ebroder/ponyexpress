b4ffcffcb7ee405a8d8011f596616cb7ef72a77d
Evan Broder <broder@mit.edu>
2009-05-29 21:03:36 -0700

    Mark the Tag class as implementing IMailbox, because hopefully it will eventually.

--- ponyexpress/model/tag.py
+++ ponyexpress/model/tag.py
@@ -4,9 +4,16 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from ponyexpress.model.base import Base
 from ponyexpress.model.message_tag import MessageTag
+from ponyexpress.model import meta
+
+from zope.interface import implements
+from twisted.mail import imap4
 
 class Tag(Base):
     __tablename__ = 'tags'
+    # For each tag, there is an associated mailbox - this class
+    # implements that interface
+    implements(imap4.IMailbox)
 
     id = sa.Column(sa.types.Integer, primary_key=True)
     name = sa.Column(sa.types.Unicode(255), nullable=False, index=True,
