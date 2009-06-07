90a3c9aca4f17d4d9e84abdb59d32952965c7096
Evan Broder <broder@mit.edu>
2009-06-07 11:15:06 -0700

    Conditionalize the monkeypatch on running against 0.5.
    This makes PonyExpress compatible with the 0.6 beta + my #1016 patch.

--- ponyexpress/model/monkeypatch.py
+++ ponyexpress/model/monkeypatch.py
@@ -12,43 +12,47 @@ change, that's not acceptable for PonyExpress.
 """
 
 import sqlalchemy
-from sqlalchemy.databases import sqlite
-import sqlalchemy.types as sqltypes
 
 # I'm monkey patching the sqlite dialect to always set AUTOINCREMENT
 # on single-column primary key fields, because I need the semantics of
 # a ROWID never being reused
-class PonySQLiteDDLCompile(sqlite.SQLiteSchemaGenerator):
-    def get_column_specification(self, column, **kwargs):
-        colspec = [self.preparer.format_column(column),
-                   column.type.dialect_impl(self.dialect).get_col_spec()]
+#
+# This will hopefully be fixed in SQLAlchemy 0.6, so let's only
+# monkeypatch 0.5.
+if sqlalchemy.__version__.startswith('0.5'):
+    from sqlalchemy.databases import sqlite
+    import sqlalchemy.types as sqltypes
+    class PonySQLiteDDLCompile(sqlite.SQLiteSchemaGenerator):
+        def get_column_specification(self, column, **kwargs):
+            colspec = [self.preparer.format_column(column),
+                       column.type.dialect_impl(self.dialect).get_col_spec()]
 
-        default = self.get_column_default_string(column)
-        if default is not None:
-            colspec.append('DEFAULT ' + default)
+            default = self.get_column_default_string(column)
+            if default is not None:
+                colspec.append('DEFAULT ' + default)
 
-        if not column.nullable:
-            colspec.append('NOT NULL')
+            if not column.nullable:
+                colspec.append('NOT NULL')
 
-        if column.primary_key and \
-                'sqlite_autoincrement' in column.table.kwargs and \
-                column.table.kwargs['sqlite_autoincrement'] and \
-                len(column.table.primary_key.columns) == 1 and \
-                isinstance(column.type, sqltypes.Integer) and \
-                not column.foreign_keys:
-            colspec.append('PRIMARY KEY AUTOINCREMENT')
+            if column.primary_key and \
+                    'sqlite_autoincrement' in column.table.kwargs and \
+                    column.table.kwargs['sqlite_autoincrement'] and \
+                    len(column.table.primary_key.columns) == 1 and \
+                    isinstance(column.type, sqltypes.Integer) and \
+                    not column.foreign_keys:
+                colspec.append('PRIMARY KEY AUTOINCREMENT')
 
-        return ' '.join(colspec)
+            return ' '.join(colspec)
 
-    def visit_primary_key_constraint(self, constraint):
-        if len(constraint.columns) == 1:
-            c = list(constraint).pop(0)
-            if c.primary_key and \
-                    'sqlite_autoincrement' in c.table.kwargs and \
-                    c.table.kwargs['sqlite_autoincrement'] and \
-                    isinstance(c.type, sqltypes.Integer) and \
-                    not c.foreign_keys:
-                return ''
-        return super(PonySQLiteDDLCompile, self).visit_primary_key_constraint(constraint)
+        def visit_primary_key_constraint(self, constraint):
+            if len(constraint.columns) == 1:
+                c = list(constraint).pop(0)
+                if c.primary_key and \
+                        'sqlite_autoincrement' in c.table.kwargs and \
+                        c.table.kwargs['sqlite_autoincrement'] and \
+                        isinstance(c.type, sqltypes.Integer) and \
+                        not c.foreign_keys:
+                    return ''
+            return super(PonySQLiteDDLCompile, self).visit_primary_key_constraint(constraint)
 
-sqlite.dialect.schemagenerator = PonySQLiteDDLCompile
+    sqlite.dialect.schemagenerator = PonySQLiteDDLCompile
--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email="broder@mit.edu",
     license="MIT",
     packages=find_packages(),
-    #install_requires=['Twisted_Mail', 'SQLAlchemy>=0.5.0,<0.6a']
+    #install_requires=['Twisted_Mail', 'SQLAlchemy>=0.5.0']
 )
